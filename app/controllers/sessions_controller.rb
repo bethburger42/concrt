36546d22ed094d7423416b19989a510ea4c736e0
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2015-11-19 15:55:46 -0800

    fixed session bug

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.authenticate login_params['email'], login_params['password']
+
+  	if(user)
+  		session[:user_id] = user.id
+  		flash[:success] = 'You have successfully logged in!'
+  		redirect_to users_path
+  	else
+  		flash[:danger] = 'Invalid email or password!'
+  		redirect_to root_path
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	flash[:success] = 'You have successfully logged out!'
+  	redirect_to root_path
+  end
+
+  private 
+
+  def login_params
+  	params.require(:user).permit(:email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,23 @@ class User < ActiveRecord::Base
   #                         foreign_key: "friend_id"
  
   # belongs_to :friend, class_name: "User"
+
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive: false},
+  email: true
+
+  validates :name,
+  presence: true,
+  length: {maximum: 20}
+
+  has_secure_password
+
+  validates_presence_of :password,
+  confirmation: true
+
+	def self.authenticate email, password
+		User.find_by_email(email).try(:authenticate, password)
+	end
+
 end
