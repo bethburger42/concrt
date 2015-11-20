9c3cec3ac1e8319c0bbe8efcb9c1b565bacda028
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2015-11-20 13:59:08 -0800

    working on users index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,8 @@ Rails.application.routes.draw do
 
   get 'users' => 'users#index'
 
+
+  resources :users
   resources :events
 
   # The priority is based upon order of creation: first created -> highest priority.
