c9b12dd21cba01fcdaff7bb17a579e7db174595d
Beth Ernsberger <bether.ga.2015@gmail.com>
2015-11-24 17:42:04 -0800

    working validation for dates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :users
-
-  # validates :datepicker_start_date, :datepicker_end_date, presence: true
+  validates :event_id, :date, :location, :artist, presence: true
 
 end
